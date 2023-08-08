a0f3a9079aa790fcd4ddeed5446a96a98598b9e3
Abid Ansari <abidraza8104@gmail.com>
2023-08-08 22:05:31 +0530

    first commit

---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,96 @@
+import Image1 from '../../assets/meesho.png'
+import Image2 from '../../assets/myblogapp.png'
+import Image3 from '../../assets/hackathon.png'
+import Image4 from '../../assets/project4.jpg'
+import Image5 from '../../assets/project5.jpg'
+import Image6 from '../../assets/resumebuilder.png'
+import Image7 from '../../assets/project7.jpg'
+import Image8 from '../../assets/project8.jpg'
+import Image9 from '../../assets/urlshortner.png'
+
+const data = [
+    {
+        id: 1,
+        category: 'uiux',
+        image: Image1,
+        title: "Meesho Ecommerce Clone",
+        desc: "The project is an e-commerce website clone of Meesho.com, designed to provide a user-friendly online shopping experience. !",
+        demo: 'https://meesho-clone-sooty.vercel.app/',
+        github: 'https://github.com/abidraza5594/meesho-clone'
+    },
+    {
+        id: 2,
+        category: 'frontend',
+        image: Image2,
+        title: "My Blog Application",
+        desc: "Designed and developed a minimalist blog application homepage using React, showcasing foundational skills in front-end web development.!",
+        demo: 'https://my-blog-app-puce-gamma.vercel.app/',
+        github: 'https://github.com/abidraza5594/My-Blog-App'
+    },
+    {
+        id: 3,
+        category: 'frontend',
+        image: Image3,
+        title: "Hackathon Submissions App (Zillancer)",
+        desc: "Developed a submissions app enabling learners to submit hackathon solutions and manage submissions effectively..!",
+        demo: 'https://hackathon-silk-psi.vercel.app/',
+        github: 'https://github.com/abidraza5594/Hackathon'
+    },
+    {
+        id: 4,
+        category: 'uiux',
+        image: Image4,
+        title: "Project Title Four (UIUX)",
+        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        demo: 'http://egatortutorials.com',
+        github: 'https://github.com/egattor'
+    },
+    {
+        id: 5,
+        category: 'uiux',
+        image: Image5,
+        title: "Project Title Five (UIUX)",
+        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
+        demo: 'http://egatortutorials.com',
+        github: 'https://github.com/egattor'
+    },
+    {
+        id: 6,
+        category: 'frontend',
+        image: Image6,
+        title: "Dynamic Resume Builder",
+        desc: "Developed a robust and user-friendly resume builder application using React.js, enhancing job seekers' ability to create professional resumes effortlessly.!",
+        demo: 'https://buildyourresume.vercel.app/',
+        github: 'https://github.com/abidraza5594/buildyourresume'
+    },
+    {
+        id: 7,
+        category: 'frontend',
+        image: Image7,
+        title: "Project Title Seven (Frontend)",
+        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        demo: 'http://egatortutorials.com',
+        github: 'https://github.com/egattor'
+    },
+    {
+        id: 8,
+        category: 'backend',
+        image: Image8,
+        title: "Content Management System (Care24)",
+        desc: "We can Add,Edit,Delete,Read Complete CRUD Base Project.This project based on Authentication and  authorization  at perticular user.Also have Search functionality so that we can search perticulat content.Description:   This is a Authentication System in this project User can Create Account and login to the website !",
+        demo: 'https://github.com/abidraza5594/',
+        github: 'https://github.com/abidraza5594/Content-Management-System'
+    },
+    {
+        id: 9,
+        category: 'frontend',
+        image: Image9,
+        title: "URL Shortner App",
+        desc: "Lorem ipsum is a name for a common type of placeholder text. Also known as filler or dummy text, this is text copy that serves to fill a space without saying anything!",
+        demo: 'https://url-sortner.vercel.app/',
+        github: 'https://github.com/abidraza5594/URL-Sortner'
+    }
+]
+
+
+export default data
